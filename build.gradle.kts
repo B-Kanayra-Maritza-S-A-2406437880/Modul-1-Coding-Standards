6506c0dac9bb0b91a668cb011473aa30fa7a9b2f
kanayradeeva010 <adeevakanayra@gmail.com>
2026-02-09 23:30:19 +0700

    Create Product Modul  Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
 	testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
 	testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
 	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+	testImplementation("org.junit.jupiter:junit-jupiter:${junitJupiterVersion}")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
 
