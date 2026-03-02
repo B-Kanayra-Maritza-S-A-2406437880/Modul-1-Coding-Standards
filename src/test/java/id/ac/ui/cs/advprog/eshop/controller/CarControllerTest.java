885f1a12d77d08b5a98fc8ca464f193ca4d2d1c5
kanayradeeva010 <adeevakanayra@gmail.com>
2026-03-02 21:57:44 +0700

    added auto configure mock mvc to car controller test

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/CarControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/CarControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CarController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class CarControllerTest {
 
     @Autowired
