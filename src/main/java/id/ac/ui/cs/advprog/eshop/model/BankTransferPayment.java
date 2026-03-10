b284c0eff62512931e17dbc07773ae9ce1bb7a3e
kanayradeeva010 <adeevakanayra@gmail.com>
2026-03-10 13:56:14 +0700

    [GREEN] Implement BankTransferPayment

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/BankTransferPayment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/BankTransferPayment.java
@@ -1,10 +1,26 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import java.util.Map;
 
 public class BankTransferPayment extends Payment {
     public BankTransferPayment(String id, String method,
                                Map<String, String> paymentData, Order order) {
         super(id, method, paymentData, order);
+        if (isValidBankTransfer(paymentData)) {
+            this.setStatus(PaymentStatus.PENDING.getValue());
+        } else {
+            this.setStatus(PaymentStatus.REJECTED.getValue());
+        }
+    }
+
+    private boolean isValidBankTransfer(Map<String, String> data) {
+        String bankName = data.get("bankName");
+        String referenceCode = data.get("referenceCode");
+        return isNotEmpty(bankName) && isNotEmpty(referenceCode);
+    }
+
+    private boolean isNotEmpty(String value) {
+        return value != null && !value.isEmpty();
     }
 }
