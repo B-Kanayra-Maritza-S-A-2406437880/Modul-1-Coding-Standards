51a16be13deefc03584af2342f7ed3f8a4501c38
kanayradeeva010 <adeevakanayra@gmail.com>
2026-03-10 10:33:46 +0700

    [GREEN] Implement Order Model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,6 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @Getter
@@ -10,17 +12,28 @@ public class Payment {
     private String status;
     private Map<String, String> paymentData;
 
+    private static final List<String> VALID_STATUSES =
+            Arrays.asList("PENDING", "SUCCESS", "REJECTED");
+
     public Payment(String id, String method,
                    Map<String, String> paymentData) {
-
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.status = "PENDING";
     }
 
     public Payment(String id, String method,
                    Map<String, String> paymentData, String status) {
-
+        this(id, method, paymentData);
+        this.setStatus(status);
     }
 
     public void setStatus(String status) {
-
+        if (!VALID_STATUSES.contains(status)) {
+            throw new IllegalArgumentException(
+                    "Invalid payment status: " + status);
+        }
+        this.status = status;
     }
 }
