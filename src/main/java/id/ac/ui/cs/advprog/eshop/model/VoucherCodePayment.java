641013fdbfd94da6c476e51ad0d5b91eb1601183
kanayradeeva010 <adeevakanayra@gmail.com>
2026-03-10 13:14:36 +0700

    [GREEN] Implement VoucherCodePayment

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/VoucherCodePayment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/VoucherCodePayment.java
@@ -1,11 +1,35 @@
-// src/main/java/id/ac/ui/cs/advprog/eshop/model/VoucherCodePayment.java
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import java.util.Map;
 
 public class VoucherCodePayment extends Payment {
     public VoucherCodePayment(String id, String method,
                               Map<String, String> paymentData, Order order) {
         super(id, method, paymentData, order);
+        if (validateVoucher(paymentData)) {
+            this.setStatus(PaymentStatus.SUCCESS.getValue());
+        } else {
+            this.setStatus(PaymentStatus.REJECTED.getValue());
+        }
+    }
+
+    private boolean validateVoucher(Map<String, String> data) {
+        String code = data.get("voucherCode");
+        if (code == null || code.isEmpty()){
+            return false;
+        }
+        if (code.length() != 16) {
+            return false;
+        }
+
+        if (!code.startsWith("ESHOP")){
+            return false;
+        }
+
+        long numericCount = code.chars()
+                .filter(Character::isDigit)
+                .count();
+        return numericCount == 8;
     }
 }
