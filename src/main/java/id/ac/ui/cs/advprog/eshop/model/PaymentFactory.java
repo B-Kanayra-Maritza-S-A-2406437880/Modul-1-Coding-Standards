ac9be1c6f7b5d83831f183c8f2554b9d1e860cf8
kanayradeeva010 <adeevakanayra@gmail.com>
2026-03-10 14:42:57 +0700

    [GREEN] Implement PaymentFactory

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentFactory.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentFactory.java
@@ -1,13 +1,23 @@
-// src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentFactory.java
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
 import org.springframework.stereotype.Component;
 import java.util.Map;
+import java.util.UUID;
 
 @Component
 public class PaymentFactory {
     public Payment create(Order order, String method,
                           Map<String, String> paymentData) {
-        return null;
+        String id = UUID.randomUUID().toString();
+
+        if (method.equals(PaymentMethod.VOUCHER_CODE.getValue())) {
+            return new VoucherCodePayment(id, method, paymentData, order);
+        }
+        if (method.equals(PaymentMethod.BANK_TRANSFER.getValue())) {
+            return new BankTransferPayment(id, method, paymentData, order);
+        }
+
+        return new Payment(id, method, paymentData, order);
     }
 }
